6ce6fb14205788ac715ad157830672def37c9e51
Ruslan <ruslanrttd@gmail.com>
2022-12-08 12:25:29 +0300

    Создание класса DB

---
+++ spawithgitend/DB.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spa
+{
+    internal class DB
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=root;database=users");
+        public void openConnection() // метод открытия доступа к базе данных
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+        public void closeConnection() // метод закрытия доступа к базе данных
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+        public MySqlConnection GetConnection() // метод возврата подключения к бд
+        {
+            return connection;
+        }
+    }
+}
