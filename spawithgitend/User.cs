c7d3de7834e4ec6f7b6a6e9339def06da19f1632
Ruslan <ruslanrttd@gmail.com>
2022-12-08 12:17:29 +0300

    Создание класса User

---
+++ spawithgitend/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spawithgitend
+{
+    static class User
+    {
+        public static string login; // поле с логином пользователя
+        public static string password; // поле с паролем пользователя
+        public static string fio; // поле с фио пользователя
+        public static string pn; // поле с номером телефона пользователя
+        public static string adress; // поле с адресом пользователя
+    }
+}
