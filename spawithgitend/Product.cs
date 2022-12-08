2e5e1fcc96d64d4d457fd6dcdfd36da94f2c3499
Ruslan <ruslanrttd@gmail.com>
2022-12-08 12:21:16 +0300

    Создание класса Product

---
+++ spawithgitend/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spawithgitend
+{
+    internal class Product
+    {
+        public static int id = 0; // id продукта, используется для функционала меню
+    }
+}
